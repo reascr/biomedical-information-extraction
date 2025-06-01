dd4a71f7b0c41de94133acd9de6fdf991edfbb3f
reascr <94176469+reascr@users.noreply.github.com>
2025-06-01 13:21:46 +0200

    changed comments

--- run_on_ucloud.sh
+++ run_on_ucloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Submit a job on ucloud
+# submit a job on ucloud
 
 LOCAL_PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SERVER_PROJECT_DIR="/home/ucloud/" # Server project directory path
@@ -15,27 +15,27 @@ scp -P $SSH_PORT -i "$SSH_KEY_PATH" -r "$LOCAL_PROJECT_DIR" ucloud@ssh.cloud.sdu
 
 ssh -i "$SSH_KEY_PATH" -p $SSH_PORT ucloud@ssh.cloud.sdu.dk << 'EOF' # SSH into the server and run commands
 
-    # Install Python 3.8 (server has python version 3.12.3. We need to downgrade to python 3.10.12)
-    export DEBIAN_FRONTEND=noninteractive  # Disable interactive prompts
+    #install Python 3.8 (server has python version 3.12.3. We need to downgrade to python 3.10.12)
+    export DEBIAN_FRONTEND=noninteractive  # disable interactive prompts
     sudo apt update -y
     sudo apt install -y software-properties-common
     sudo add-apt-repository -y ppa:deadsnakes/ppa
     sudo apt install -y python3.10 python3.10-venv python3.10-dev
 
-    # Navigate to the project directory
+    # go to the project directory
     cd /home/ucloud/biomedical-information-extraction 
 
-    # Set up virtual environment with Python 3.10
-    python3.10 -m venv venv  # Create virtual environment with Python 3.10
-    source venv/bin/activate  # Activate the virtual environment
+    # set up virtual environment with Python 3.10
+    python3.10 -m venv venv
+    source venv/bin/activate  
 
-    # Install required dependencies from the requirements.txt
+    # install requirements
     pip install -r requirements.txt
 
     # Run the script in the background using nohup (write output to both output.log and terminal)
     echo "Running the script in the background..."
     # nohup python3 scripts/optuna_NER.py 2>&1 | tee output.log &
-    echo "Job started in the background." # If not using this script to connect to the server, make sure to activate the venv before running the code
-
+    echo "Job started in the background." 
+    
 EOF
 
